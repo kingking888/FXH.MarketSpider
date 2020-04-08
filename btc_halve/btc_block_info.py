e5c324b6a92d914495a89c61f9afe661ed2cc4bf
ganqiyin <ganqiyin@wxdl.com>
2020-04-08 17:05:57 +0800

    add next_halve_time

--- btc_halve/btc_block_info.py
+++ btc_halve/btc_block_info.py
@@ -19,20 +19,22 @@ headers1 = {
 }
 
 headers2 = {
-	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-	"Accept-Encoding": "gzip, deflate, br",
-	"Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-	"Cache-Control": "max-age=0",
-	"Connection": "keep-alive",
-	"Cookie": "_ga=GA1.2.1244702135.1576054842; intercom-id-yttqkdli=13951abc-335d-422b-a82e-91738c393c52; _globalGA=GA1.2.1244702135.1576054842; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216f0df8ebec652-08d5742f329bb5-6701434-2073600-16f0df8ebedb02%22%2C%22%24device_id%22%3A%2216f0df8ebec652-08d5742f329bb5-6701434-2073600-16f0df8ebedb02%22%2C%22props%22%3A%7B%7D%7D; acw_tc=2760825e15788329245613124e099621fdac97a89428a4b28ec16e28317751; _gid=GA1.2.732179981.1578997835; _globalGA_gid=GA1.2.955375280.1578997835; io=qNLvlYryHklF-_48AJ2D; acw_sc__v2=5e1e725fd7bb6b2c93c939b464539306070f3ef6; acw_sc__v3=5e1e72604872fb483c2862e9f20890415d8e2c77; XSRF-TOKEN=eyJpdiI6Ilg5Q2hLbkhoakllWFVVRjh1T3djdXc9PSIsInZhbHVlIjoiV0h0S3RtU01IMGE3Q21NMUgxSWJ6K2pPeG5hTU8xcHBKRXY3MmtPV3F2SjVCT3hwSUlPMGJlcGF4Z2RlNCs1WCIsIm1hYyI6ImM5YTI1MDhhNWZlMTI0NGE1MDgzZGE2ZGM4OGY3MWYxZDRlZmRmMWQ2Y2ZkYzUzNGNiYWE3NmIyNmQ5MTA4OWEifQ%3D%3D; laravel_session=eyJpdiI6IjBPRE42bXdGdjJHNnZYRm92dXJnM0E9PSIsInZhbHVlIjoiWk1TM1V5b2ZpdVZVZFRcL1Nic3VOdHFqdkhwMm00SnN3aWVENUFwWkRpZ0xrcHNQeFdyWmpvNUs2MlFLMWh6RFYiLCJtYWMiOiI0ZjNlOWE0ZTYyMDE0M2JkNDczMGUzZmI1MTU3NjQyNTk5NWZkOTQxY2QzNTQ3NmQ5ODcyMmMxM2JhNzJmNGRjIn0%3D; LeHHli2MjjevYkpabW4Ol0edXbKQFh1Xv2q5KjBl=eyJpdiI6Im5vS1FJOE5IZDJWMzFsaTFNNHJJbXc9PSIsInZhbHVlIjoid0E2dTZSTmZWY0tGRFhVd2k4bzRJeHNIOTVSMVRreVRsR2NTUmlHR05Ha0NnN0VDeDlhMTNES1lkZDN0OFZoK2p4MSt4M0ZwZ0QzTkY0dHhxM05iVkRYcERORjBSUUYyRnYyTVJCMlIrQlUrOGE1NGc3eVN6ZURnQTBLUmtjUExWMnROK0dlUVlvTlpNd0NuRTltenQwcUVxdWxMK1B2eUt5MHFzXC9YMDREa05lNGpScVJWTG1kVG9VcmFpTXdpTnhzaHJCanpZempZVmRmMndNZlBSK3AzY1E2YlRITUlYeE8rRmc2WXVVbGhIekl2VEtHdGdrcDRnVU04emsyRjI0TGlHdCtEUzdEV0RzcllyWmh1cW9YT1wvTXhaVGNjOWI5ZHRXTFRYZlREWm9cLzF4dGhBR2ZvV1ZTaE80WEFFYkgiLCJtYWMiOiJhY2I4NjExMWY3MmFkMWYzZWYzNDFmODc3ZGEyZmZiNTM0ODdlMGU3NmFhNjQ2ZTU1ZmMwYzI2ZmViNDEwYjNiIn0%3D",
-	"Host": "btc.com",
-	"Upgrade-Insecure-Requests": "1",
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Cookie": "_ga=GA1.2.1244702135.1576054842; intercom-id-yttqkdli=13951abc-335d-422b-a82e-91738c393c52; _globalGA=GA1.2.1244702135.1576054842; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216f0df8ebec652-08d5742f329bb5-6701434-2073600-16f0df8ebedb02%22%2C%22%24device_id%22%3A%2216f0df8ebec652-08d5742f329bb5-6701434-2073600-16f0df8ebedb02%22%2C%22props%22%3A%7B%7D%7D; acw_tc=2760825815863112314588210e2bcb838cf02e5312110188d0ed38629eb169; _gid=GA1.2.1985573557.1586311234; _globalGA_gid=GA1.2.683565384.1586311234; hideHeaderLayer=false; acw_sc__v2=5e8d8ef1400d64d50e702341c66486e2fbe9b567; _gat=1; _gat_globalGA=1; io=zNYIMKbXACkfLsIkA3lc; acw_sc__v3=5e8d915f66ffafaa4ff8ca2779898ce651243b61; XSRF-TOKEN=eyJpdiI6IjV5WVZtZ2pjYlZ2VFlMRzBBTjZBMEE9PSIsInZhbHVlIjoicXphbE1HSlVNS0MyclYzbjhoUjBsRFNEeTQ4OGVCMDZjbk5lbUFKUDJyQTczQXNQeTZZZnpuRFM5MUpmUXJIUiIsIm1hYyI6IjU1ZDJkY2IyNWYyMGFhZjM3N2UwOGU2OGE0NTI3MmFiZjIyMzgyYTNjNDZiZjMyMmI4MzQ4MTMyN2VmOGIxOTgifQ%3D%3D; laravel_session=eyJpdiI6ImpmOVJKd2pER0lkeTRUOXRsaGtwRXc9PSIsInZhbHVlIjoiNnhPaFMrUGNpcDNYWFN0NGFLblJmWW1zZU1xT0hmbDRwU2VuSmlsa3lKUUd5WnF4ckxTUjBKVCtkUHRoMjFlTiIsIm1hYyI6IjBiZGJmOWJkYmNjM2ZlMjAxZmZkNzBmOTdmNTY4ZTNmNWIzNGVmYWFlZDdiODRhNTg5M2E1YTYzN2ZhNzExY2IifQ%3D%3D",
+    "Host": "btc.com",
+    "Referer": "https://btc.com/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"
 }
 
 def send_request():
     url1 = "https://chain.api.btc.com/v3/block/latest"
     url2 = "https://btc.com/"
+    url3 = "https://btc.com/stats/unconfirmed-tx"
 
     proxies = {"https": "http://127.0.0.1:{}".format(random.randint(8080, 8323))}
     resp1 = requests.get(url1, headers=headers1, proxies=proxies)
@@ -43,7 +45,7 @@ def send_request():
     new_data = parse_response(resp2)
 
     proxies = {"https": "http://127.0.0.1:{}".format(random.randint(8080, 8323))}
-    obj2 = etree.HTML(requests.get("https://btc.com/stats/unconfirmed-tx", headers=headers2, proxies=proxies).content)
+    obj2 = etree.HTML(requests.get(url3, headers=headers2, proxies=proxies).content)
     best_fees = obj2.xpath('//b[@id="fees_recommended_bk"]/text()')[0].strip() + " BTC/KvB"
 
     save_data(data, new_data, best_fees)
@@ -71,6 +73,8 @@ def parse_response(resp):
     t = "".join(obj.xpath("//div[@class='panel-body']/ul[2]/li[2]/dl/dd//text()")).replace("\n", "").replace(" ", "")
     d = t.replace("天", ".").replace("小时", "").split(".")
     new_data['next_difficulty_date'] = int(time.time()) + int(d[0]) * 86400 + int(d[1]) * 3600
+    # 预测减半时间
+    new_data['next_halve_time'] = obj.xpath("//div[@class='panel-body']/ul[3]/li/dl/dd/div[1]/span[2]/text()")[0]
 
     return new_data
 
@@ -95,6 +99,7 @@ def save_data(data, new_data, best_fees):
         "difficulty": new_data.get('difficulty'),
         "next_difficulty_estimated": new_data.get('next_difficulty_estimated'),
         "next_difficulty_date": new_data.get('next_difficulty_date'),
+        "next_halve_time": new_data.get("next_halve_time"),
     }
 
     while True:
